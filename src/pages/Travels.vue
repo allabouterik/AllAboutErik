5e8debc463c39901b40a19926110ac4b630791b3
Eoin <eoinom@gmail.com>
2022-09-16 00:24:00 +0100

    Turn down max volume on Travels page

--- src/pages/Travels.vue
+++ src/pages/Travels.vue
@@ -6,6 +6,7 @@
       :audioDuration="$page.Travels.edges[0].node.bgAudioDuration"
       :audioFadeInDuration="$page.Travels.edges[0].node.bgAudioFadeInDuration"
       :audioFadeOutDuration="$page.Travels.edges[0].node.bgAudioFadeOutDuration"
+      :maxVolume=0.9
       :playMusic="playMusic"
     />
 
