648852dd29932eea8b6fd35bee40c90a8d22d585
Eoin <eoinom@gmail.com>
2022-12-16 00:14:08 +0000

    Cypress tests for Publications flipbook actions

--- cypress/e2e/publications_pages.cy.js
+++ cypress/e2e/publications_pages.cy.js
@@ -119,5 +119,292 @@ describe('Publications - Individual Pages', () => {
         .and('have.prop', 'naturalWidth')
         .should('be.greaterThan', 0);
     });
+
+    it(`${publication.title} has a flipbook with ${publication.pages} pages`, () => {
+      cy.get('#flipbookContainer')
+        .find('.page-num')
+        .should('contain.text', `Page 1 of ${publication.pages}`);
+    });
+
+    it(`the ${publication.title} flipbook starts at 1x scale`, () => {
+      cy.get('#flipbookContainer .viewport')
+        .find('.container')
+        .should('have.css', 'transform', 'matrix(1, 0, 0, 1, 0, 0)');
+    });
+
+    it(`the ${publication.title} flipbook has the correct icons enabled and disabled`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#firstpage_icon')
+        .should('have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#left_icon')
+        .should('have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#minus_icon')
+        .should('have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#plus_icon')
+        .should('not.have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#right_icon')
+        .should('not.have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#lastpage_icon')
+        .should('not.have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreen_icon')
+        .should('not.have.class', 'disabled');
+    });
+
+    it(`clicking the ${publication.title} flipbook plus btn increases it to 2x scale and enables the minus btn`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#plus_icon')
+        .click();
+
+      cy.get('#flipbookContainer .viewport')
+        .find('.container')
+        .should('have.css', 'transform', 'matrix(2, 0, 0, 2, 0, 0)');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#minus_icon')
+        .should('not.have.class', 'disabled');
+    });
+
+    it(`clicking the ${publication.title} flipbook plus btn again increases it to 4x scale and disables the plus btn`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#plus_icon')
+        .click();
+
+      cy.get('#flipbookContainer .viewport')
+        .find('.container')
+        .should('have.css', 'transform', 'matrix(4, 0, 0, 4, 0, 0)');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#plus_icon')
+        .should('have.class', 'disabled');
+    });
+
+    it(`clicking the ${publication.title} flipbook minus btn decreases it to 2x scale and enables the plus btn`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#minus_icon')
+        .click();
+
+      cy.get('#flipbookContainer .viewport')
+        .find('.container')
+        .should('have.css', 'transform', 'matrix(2, 0, 0, 2, 0, 0)');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#plus_icon')
+        .should('not.have.class', 'disabled');
+    });
+
+    it(`clicking the ${publication.title} flipbook minus btn again decreases it to 1x scale and disables the minus btn`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#minus_icon')
+        .click();
+
+      cy.get('#flipbookContainer .viewport')
+        .find('.container')
+        .should('have.css', 'transform', 'matrix(1, 0, 0, 1, 0, 0)');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#minus_icon')
+        .should('have.class', 'disabled');
+    });
+
+    it(`clicking the ${publication.title} flipbook right btn increases it to page 2`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#right_icon')
+        .click();
+
+      cy.get('#flipbookContainer')
+        .find('.page-num')
+        .should('contain.text', `Page 2 of ${publication.pages}`);
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#left_icon')
+        .should('not.have.class', 'disabled');
+    });
+
+    it(`clicking the ${publication.title} flipbook last page btn increases the page number`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#lastpage_icon')
+        .click();
+
+      cy.get('#flipbookContainer')
+        .find('.page-num')
+        .should('contain.text', `Page 8 of ${publication.pages}`);
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#stop_icon_end')
+        .should('exist');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#lastpage_icon')
+        .should('not.exist');
+    });
+
+    it(`clicking the ${publication.title} flipbook pause btn stops increasing the pages`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#stop_icon_end')
+        .click();
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#stop_icon_end')
+        .should('not.exist');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#lastpage_icon')
+        .should('exist');
+    });
+
+    it(`can skip to the last page of the ${publication.title} flipbook via the url hash`, () => {
+      cy.visit(`${publication.link}#${publication.pages}`);
+
+      cy.get('#flipbookContainer')
+        .find('.page-num')
+        .should(
+          'contain.text',
+          `Page ${publication.pages} of ${publication.pages}`
+        );
+    });
+
+    it(`the ${publication.title} flipbook has the correct icons enabled and disabled for the last page`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#firstpage_icon')
+        .should('not.have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#left_icon')
+        .should('not.have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#minus_icon')
+        .should('have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#plus_icon')
+        .should('not.have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#right_icon')
+        .should('have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#lastpage_icon')
+        .should('have.class', 'disabled');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreen_icon')
+        .should('not.have.class', 'disabled');
+    });
+
+    it(`clicking the ${publication.title} flipbook left btn decreases the page by 1`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#left_icon')
+        .click();
+
+      cy.get('#flipbookContainer')
+        .find('.page-num')
+        .should(
+          'contain.text',
+          `Page ${publication.pages - 1} of ${publication.pages}`
+        );
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#right_icon')
+        .should('not.have.class', 'disabled');
+    });
+
+    it(`clicking the ${publication.title} flipbook first page btn decreases the page number`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#firstpage_icon')
+        .click();
+
+      cy.get('#flipbookContainer')
+        .find('.page-num')
+        .should(
+          'contain.text',
+          `Page ${publication.pages - 8} of ${publication.pages}`
+        );
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#stop_icon_start')
+        .should('exist');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#firstpage_icon')
+        .should('not.exist');
+    });
+
+    it(`clicking the ${publication.title} flipbook pause btn stops decreasing the pages`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#stop_icon_start')
+        .click();
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#stop_icon_start')
+        .should('not.exist');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#firstpage_icon')
+        .should('exist');
+    });
+
+    it(`clicking the ${publication.title} flipbook fullscreen btn enables fullscreen mode`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreenExit_icon')
+        .should('not.exist');
+
+      cy.get('#flipbookContainer').should(
+        'not.have.class',
+        'fullscreenContainer'
+      );
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreen_icon')
+        .click();
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreen_icon')
+        .should('not.exist');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreenExit_icon')
+        .should('exist');
+
+      cy.get('#flipbookContainer').should('have.class', 'fullscreenContainer');
+    });
+
+    it(`clicking the ${publication.title} flipbook exit fullscreen btn disables fullscreen mode`, () => {
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreenExit_icon')
+        .should('exist');
+
+      cy.get('#flipbookContainer').should('have.class', 'fullscreenContainer');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreenExit_icon')
+        .click();
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreenExit_icon')
+        .should('not.exist');
+
+      cy.get('#flipbookContainer .action-bar')
+        .find('#fullscreen_icon')
+        .should('exist');
+
+      cy.get('#flipbookContainer').should(
+        'not.have.class',
+        'fullscreenContainer'
+      );
+    });
   });
 });
