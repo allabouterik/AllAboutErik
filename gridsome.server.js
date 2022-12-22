9a21fc301b51608c08c38294d1e882b414d75b2a
Eoin <eoinom@gmail.com>
2022-12-22 12:33:12 +0000

    Format gridsome.server.js file

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,19 +5,19 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = function (api) {
-  api.loadSource(actions => {
+  api.loadSource((actions) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api
-  })
+  });
 
   // api.createPages(({ createPage }) => {
   //   // Use the Pages API here: https://gridsome.org/docs/pages-api
   // })
 
-  const slugify = require('@sindresorhus/slugify')
+  const slugify = require('@sindresorhus/slugify');
 
   // create the individual Friends pages
   api.createManagedPages(async ({ graphql, createPage, removePageByPath }) => {
@@ -71,135 +71,144 @@ module.exports = function (api) {
 
   // create the individual Collections pages
   api.createPages(async ({ graphql, createPage }) => {
-    const { data } = await graphql(`{
-      allCollections {
-        edges {
-          node {
-            id
-            title
+    const { data } = await graphql(`
+      {
+        allCollections {
+          edges {
+            node {
+              id
+              title
+            }
           }
         }
       }
-    }`)
+    `);
     data.allCollections.edges.forEach(({ node }) => {
-      pageSlug = slugify(node.title)
-      if (pageSlug === 'old-time-sportsmen')
-        return;
+      pageSlug = slugify(node.title);
+      if (pageSlug === 'old-time-sportsmen') return;
       createPage({
         path: `/collections/${pageSlug}`,
         component: './src/templates/Collection.vue',
         context: {
-          id: node.id
-        }
-      })
-    })
-  })
-
+          id: node.id,
+        },
+      });
+    });
+  });
 
   // create the individual Publications pages
   api.createPages(async ({ graphql, createPage }) => {
-    const { data } = await graphql(`{
-      allPublications {
-        edges {
-          node {
-            id
-            title
+    const { data } = await graphql(`
+      {
+        allPublications {
+          edges {
+            node {
+              id
+              title
+            }
           }
         }
       }
-    }`)
+    `);
     data.allPublications.edges.forEach(({ node }) => {
-      pageSlug = slugify(node.title)
+      pageSlug = slugify(node.title);
       createPage({
         path: `/publications/${pageSlug}`,
         component: './src/templates/Publication.vue',
         context: {
-          id: node.id
-        }
-      })
-    })
-  })
-
+          id: node.id,
+        },
+      });
+    });
+  });
 
   // create the individual Archives pages
   api.createPages(async ({ graphql, createPage }) => {
-    const { data } = await graphql(`{
-      allArchives {
-        edges {
-          node {
-            id
-            title
+    const { data } = await graphql(`
+      {
+        allArchives {
+          edges {
+            node {
+              id
+              title
+            }
           }
         }
       }
-    }`)
+    `);
     data.allArchives.edges.forEach(({ node }) => {
-      pageSlug = slugify(node.title, {customReplacements: [['\'', '']]})
+      pageSlug = slugify(node.title, { customReplacements: [["'", '']] });
       createPage({
         path: `/archives/${pageSlug}`,
         component: './src/templates/Archive.vue',
         context: {
-          id: node.id
-        }
-      })
-    })
-  })
+          id: node.id,
+        },
+      });
+    });
+  });
 
   // create the individual Narrative Archive pages
   api.createPages(async ({ graphql, createPage }) => {
-    const { data } = await graphql(`{
-      allArchivesNarrative {
-        edges {
-          node {
-            id
-            title
+    const { data } = await graphql(`
+      {
+        allArchivesNarrative {
+          edges {
+            node {
+              id
+              title
+            }
           }
         }
       }
-    }`)
+    `);
     data.allArchivesNarrative.edges.forEach(({ node }) => {
-      pageSlug = slugify(node.title, {customReplacements: [['\'', '']]})
+      pageSlug = slugify(node.title, { customReplacements: [["'", '']] });
       createPage({
         path: `/archives/${pageSlug}`,
         component: './src/templates/ArchiveNarrative.vue',
         context: {
-          id: node.id
-        }
-      })
-    })
-  })
+          id: node.id,
+        },
+      });
+    });
+  });
 
   // create the individual Narrative Archive Gallery pages
   api.createPages(async ({ graphql, createPage }) => {
-    const { data } = await graphql(`{
-      allArchivesNarrativeGallery {
-        edges {
-          node {
-            id
-            title
+    const { data } = await graphql(`
+      {
+        allArchivesNarrativeGallery {
+          edges {
+            node {
+              id
+              title
+            }
           }
         }
       }
-    }`)
+    `);
     data.allArchivesNarrativeGallery.edges.forEach(({ node }) => {
-      pageSlug = slugify(node.title, {customReplacements: [['\'', '']]})
+      pageSlug = slugify(node.title, { customReplacements: [["'", '']] });
       createPage({
         path: `/archives/${pageSlug}-gallery`,
         component: './src/templates/ArchiveNarrativeGallery.vue',
         context: {
-          id: node.id
-        }
-      })
-    })
-  })
+          id: node.id,
+        },
+      });
+    });
+  });
 
   // configure webpack to use bundle analyzer plugin (ref: https://medium.com/js-dojo/how-to-reduce-your-vue-js-bundle-size-with-webpack-3145bf5019b7)
   // reduce bundle size ref: https://www.codegram.com/blog/improving-a-gridsome-website-performance/
   // (api.chainWebpack ref: https://gridsome.org/docs/server-api/#apichainwebpackfn)
   // ref: https://github.com/webpack-contrib/webpack-bundle-analyzer/blob/master/README.md
-  api.chainWebpack(config => {
+  api.chainWebpack((config) => {
     config
       .plugin('BundleAnalyzerPlugin')
-      .use(BundleAnalyzerPlugin, [{ analyzerMode: 'static', openAnalyzer: false }])
-  })
-}
+      .use(BundleAnalyzerPlugin, [
+        { analyzerMode: 'static', openAnalyzer: false },
+      ]);
+  });
+};
