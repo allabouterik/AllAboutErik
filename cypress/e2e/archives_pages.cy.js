2b9797e83bab66007464c7f70f72b3be0704f54b
Eoin <eoinom@gmail.com>
2022-12-22 22:25:02 +0000

    Additional Cypress tests for Archive Gallery template page (my-dad-earl-gallery)

--- cypress/e2e/archives_pages.cy.js
+++ cypress/e2e/archives_pages.cy.js
@@ -130,11 +130,93 @@ describe('Archives - Individual Pages', () => {
     }
 
     if (archive.isNarrativePage) {
-      it(`the ${archive.title} has the scroll for recollections header text`, () => {
+      it(`${archive.title} has the scroll for recollections header text`, () => {
         cy.get('.headerText')
           .should('contain.text', 'SCROLL')
           .and('contain.text', 'TO READ MY')
           .and('contain.text', 'RECOLLECTIONS');
+      });
+    }
+
+    if (archive.hasSeparateGalleryPage) {
+      it(`${archive.title} has a link to a separate gallery page`, () => {
+        cy.get('.headerText')
+          .should('contain.text', 'CLICK')
+          .and('contain.text', 'TO VIEW THE GALLERY');
+
+        cy.get('header')
+          .find('a.headerOverlay.link')
+          .should('have.attr', 'href')
+          .should('include', `${archive.link}-gallery`);
+      });
+
+      it(`${archive.title} gallery page successfully loads`, () => {
+        cy.get('header').find('a.headerOverlay.link').click().wait(6000);
+
+        cy.url().should('include', `${archive.link}-gallery`);
+      });
+
+      it(`${archive.title} gallery page has the scroll to view the gallery header text`, () => {
+        cy.get('.headerText')
+          .should('contain.text', 'SCROLL')
+          .and('contain.text', 'TO VIEW THE GALLERY');
+      });
+
+      it(`${archive.title} gallery page has link back to narrative page`, () => {
+        cy.get('.headerText')
+          .should('contain.text', 'CLICK')
+          .and('contain.text', 'TO READ MY')
+          .and('contain.text', 'RECOLLECTIONS');
+
+        cy.get('header')
+          .find('a.headerOverlay.link')
+          .should('have.attr', 'href')
+          .should('include', `${archive.link}`);
+      });
+
+      it(`${archive.title} gallery page contains ${archive.galleryPgNumImages} images`, () => {
+        cy.get('#mainContent .galleryBox img')
+          .should('have.length', archive.galleryPgNumImages)
+          .and('have.prop', 'naturalWidth')
+          .should('be.greaterThan', 0);
+      });
+
+      it(`${archive.title} gallery page has a link back to archives menu`, () => {
+        cy.get('header')
+          .find('a.backToArchives')
+          .should('have.attr', 'href')
+          .should('include', '/archives/menu');
+      });
+
+      it(`${archive.title} gallery page has the navigation image for back to archives menu`, () => {
+        cy.get('header a.backToArchives')
+          .find('img.backToArchivesImg')
+          .should('be.visible')
+          .should('have.attr', 'src')
+          .should('include', 'back-to-archives-single-line.png');
+
+        cy.get('header a.backToArchives')
+          .find('img.backToArchivesImg')
+          .should('be.visible')
+          .and('have.prop', 'naturalWidth')
+          .should('be.greaterThan', 0);
+      });
+
+      it(`${archive.title} gallery page has the navigation image on hover for back to archives menu`, () => {
+        cy.get('header a.backToArchives')
+          .realHover()
+          .find('img.backToArchivesImg-hover')
+          .should('have.attr', 'src')
+          .should('include', 'back-to-archives-single-line-yellow.png');
+
+        cy.get('header a.backToArchives')
+          .realHover()
+          .find('img.backToArchivesImg-hover')
+          .should('be.visible')
+          .and('have.prop', 'naturalWidth')
+          .should('be.greaterThan', 0);
+
+        cy.get('body').realHover({ position: 'topLeft' }); // reset hovering by putting mouse away
       });
     }
   });
